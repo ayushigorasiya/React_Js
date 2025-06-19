6db5e710c7f5bcb72ad8277d52af008df69f7b80
Ayushi Gorasiya <ayushigorasiya287@gmail.com>
2025-06-19 19:22:42 +0530

    Delete Redux__CRUD directory

--- Redux__CRUD/Crud/src/Redux/Reducer/IndexReducer.js
+++
@@ -1,5 +0,0 @@
-import Crud from "./CrudReducer";
-import { combineReducers  } from "redux";
-const rootCounter = combineReducers ({ Crud })
-
-export default rootCounter
--- Redux__CRUD/Crud/src/Redux/Store.js
+++
@@ -1,5 +0,0 @@
-import rootCrud from "./Reducer/CrudReducer";
-import { createStore } from "redux";
-const store = createStore(rootCrud) 
-
-export default store
